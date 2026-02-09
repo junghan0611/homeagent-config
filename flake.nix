cad75bc8638f446c4b24e6b20240b8ca3ebf951b
junghan <junghanacs@gmail.com>
2026-02-09 12:05:54 +0900

    fix: nixpkgs unstable 복원 (glibc 2.42 sstate 호환)
    nixos-25.11 (glibc 2.40) 전환 시 기존 sstate-cache와
    glibc 버전 불일치로 do_image_wic 실패

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "HomeAgent Config - RPi5 Yocto Build Environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
